e577ebd03f0ab45149a5d6f95cb5c901bca7ca2f
Thiago França da Silva <contact@tfs.dev.br>
2023-07-21 21:03:11 -0300

    fix(home): import router

--- frontend/src/home/HomeQuickActions.vue
+++ frontend/src/home/HomeQuickActions.vue
@@ -9,7 +9,7 @@
 		<div class="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
 			<div
 				class="col-span-1 flex cursor-pointer items-center justify-between rounded border border-transparent bg-white p-4 shadow transition-all hover:border-gray-400"
-				@click="$router.push('/data-source#new')"
+				@click="router.push('/data-source#new')"
 			>
 				<div class="text-lg font-medium text-gray-900">Connect Data Source</div>
 				<div class="rounded bg-gray-100 p-1">
@@ -18,7 +18,7 @@
 			</div>
 			<div
 				class="col-span-1 flex cursor-pointer items-center justify-between rounded border border-transparent bg-white p-4 shadow transition-all hover:border-gray-400"
-				@click="$router.push('/query#new')"
+				@click="router.push('/query#new')"
 			>
 				<div class="text-lg font-medium text-gray-900">Create a Query</div>
 				<div class="rounded bg-gray-100 p-1">
@@ -27,7 +27,7 @@
 			</div>
 			<div
 				class="col-span-1 flex cursor-pointer items-center justify-between rounded border border-transparent bg-white p-4 shadow transition-all hover:border-gray-400"
-				@click="$router.push('/dashboard#new')"
+				@click="router.push('/dashboard#new')"
 			>
 				<div class="text-lg font-medium text-gray-900">Create a Dashboard</div>
 				<div class="rounded bg-gray-100 p-1">
@@ -37,3 +37,10 @@
 		</div>
 	</div>
 </template>
+
+<script setup>
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+</script>
